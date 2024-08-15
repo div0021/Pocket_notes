85d4bcb78948b799b7dc541f0844b1977d273b5c
div0021 <div2wk@gmail.com>
2024-08-15 15:23:05 +0530

    'end to end protection' is added

--- src/components/Instruction.jsx
+++ src/components/Instruction.jsx
@@ -4,7 +4,7 @@ const Instruction = () => {
 
   const {selectedGroup} = useContextState();
   return (
-    <div className={`w-full h-screen justify-center items-center ${selectedGroup.length === 0 ? 'flex' : 'hidden'}`}>
+    <div className={`w-full h-screen relative justify-center items-center ${selectedGroup.length === 0 ? 'flex' : 'hidden'}`}>
     <div className='flex justify-center items-center flex-col space-y-4'>
     <img src='./initail.png' alt='img'/>
 
@@ -16,6 +16,12 @@ const Instruction = () => {
     Use Pocket Notes on up to 4 linked devices and 1 mobile phone
     </p>
 
+    </div>
+    <div className='absolute bottom-2 left-0 w-full flex justify-center items-center gap-x-3 text-sm font-normal text-[#292929]'>
+    <img alt='lock' src='./lock.png' className='w-3 h-4'/>
+    <span>
+    end-to-end encrypted
+    </span>
     </div>
     </div>
   )
